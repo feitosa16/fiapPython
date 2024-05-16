fb7678f39e3a69debd11490153ff9f6ff5a2a233
Heloisa Feitosa <103234249+feitosa16@users.noreply.github.com>
2024-05-16 01:19:24 +0000

    My thirth commit

--- condicional.py
+++ condicional.py
@@ -1,7 +1,10 @@
 nome = input ("Digite o nome: ")
 idade= int(input("Digite a idade: "))
+doencaInfectocontagiosa = input("Suspeita de doença infectocontagiosa? "). upper()
 if idade >= 65: #utilizar bem as identações principalmente por não utilizar parenteses e representar a hierarquia do programa
     print ("O paciente " + nome + " possui atendimento prioritário!")
+elif doencaInfectocontagiosa == "SIM": 
+    print("O paciente " + nome + " deve ser direcionado para a sala de espera reservada")
 else: 
     print("O paciente " + nome + "NÃO possui atendimento prioritário")
 
