13c4ac466bc10989886c5ad3bdbc9cdf66345c48
Heloisa Feitosa <103234249+feitosa16@users.noreply.github.com>
2024-05-16 01:42:25 +0000

    My first commit

---
+++ decisaoWhile.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um número: "))
+while numero < 100: #verifica a veracidade de uma condição
+    print("\t" + str(numero)) #transformar o numero em string
+    numero = numero + 1
+print("Laço encerrado...")
