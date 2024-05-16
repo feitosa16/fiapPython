7ed55511eca0601f4cd61a9ea1b35ae4ea4354ad
Heloisa Feitosa <103234249+feitosa16@users.noreply.github.com>
2024-05-16 01:11:15 +0000

    My second commit

--- condicional.py
+++ condicional.py
@@ -1,4 +1,9 @@
-nome = input ("Digite o nome ")
+nome = input ("Digite o nome: ")
 idade= int(input("Digite a idade: "))
-if idade >= 65: 
+if idade >= 65: #utilizar bem as identações principalmente por não utilizar parenteses e representar a hierarquia do programa
     print ("O paciente " + nome + " possui atendimento prioritário!")
+else: 
+    print("O paciente " + nome + "NÃO possui atendimento prioritário")
+
+#hierarquia do mesmo nivel que if e else o que faz aparece na impressão em ambos os casos
+print("FIM")
